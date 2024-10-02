09c2534cdfd1892d532befed760cfee54b63aa7a
Carlos Giovano <carlosgiovano@outlook.com>
2024-10-02 12:51:48 -0300

    ordem das rotas ajustadas

--- src/routes/AlunoTurmaRouter.js
+++ src/routes/AlunoTurmaRouter.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const alunoTurmaController = require('../controllers/AlunoTurmaController');
 
-router.get('/', alunoTurmaController.Obter);
+
+router.get('/alunos-sem-turma/', alunoTurmaController.ObterAlunosSemTurma);
 router.get('/:id', alunoTurmaController.ObterAlunosPorTurma);
+router.get('/', alunoTurmaController.Obter);
+
 router.post('/cadastrar/', alunoTurmaController.Criar);
 router.delete('/deletar/', alunoTurmaController.Deletar);
 
