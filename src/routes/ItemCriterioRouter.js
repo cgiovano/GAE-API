7467923752af0b18f6d936b6a3062098d465c602
Carlos Giovano <carlosgiovano@outlook.com>
2024-10-07 20:48:35 -0300

    Adicionado um gerenciamento de rota básico para o item criterio

---
+++ src/routes/ItemCriterioRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const itemCriterioController = require('../controllers/ItemCriterioController.js');
+
+router.get('/', itemCriterioController.Obter);
+router.post('/', itemCriterioController.Criar);
+router.update('/:id', itemCriterioController.Atualizar);
+router.delete('/:id', itemCriterioController.Deletar);
+
+module.exports = router;
