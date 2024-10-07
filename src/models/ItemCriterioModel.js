6967ba29580406a84a8b9cd7eccc42a606fb157e
Carlos Giovano <carlosgiovano@outlook.com>
2024-10-07 20:14:57 -0300

    Adicionado modelo para o item do criterio

---
+++ src/models/ItemCriterioModel.js
@@ -0,0 +1,27 @@
+const database = require('../dbconfig');
+const { DataTypes } = require('sequelize');
+
+const ItemCriterio = database.define('ItemCriterio', {
+    id: {
+        type: DataTypes.INTEGER, 
+        primaryKey: true, 
+        autoIncrement: true
+    }, 
+    id_criterio: {
+        type: DataTypes.INTEGER, 
+        references: {
+            model: 'Criterio', 
+            key: 'id'
+        }
+    }, 
+    descricao: {
+        type: DataTypes.STRING, 
+        allowNull: false
+    }, 
+    valor: {
+        type: DataTypes.FLOAT, 
+        allowNull: false
+    }
+});
+
+module.exports = ItemCriterio;
