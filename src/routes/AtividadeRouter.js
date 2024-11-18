92110d3c446db5df52e8130126643e5bc6b7cdd0
Carlos Giovano <carlosgiovano@outlook.com>
2024-11-17 23:17:52 -0300

    Adicionado tratamento de rotas para atividade

--- src/routes/AtividadeRouter.js
+++ src/routes/AtividadeRouter.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const atividadeController = require('../controllers/AtividadeController');
 
 router.get('/', atividadeController.Listar);
-router.get('/', atividadeController.Listar);
 router.post('/', atividadeController.Criar);
 router.put('/:id', atividadeController.Atualizar);
 router.delete('/:id', atividadeController.Deletar);
