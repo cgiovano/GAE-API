48fd9e1c73de84d172d04c8de3b8605faab2c638
carlosgiovano@outlook.com <carlosgiovano@outlook.com>
2025-01-03 09:37:29 -0300

    rota retrabalhada

--- src/routes/AlunoRouter.js
+++ src/routes/AlunoRouter.js
@@ -4,8 +4,8 @@ const alunosController = require('../controllers/AlunoController.js');
 
 router.get('/', alunosController.Listar);
 router.get('/:id', alunosController.ObterItem);
-router.post('/cadastrar/', alunosController.Criar);
-router.put('/editar/:id', alunosController.Atualizar);
+router.post('/', alunosController.Criar);
+router.put('/:id', alunosController.Atualizar);
 router.delete('/:id', alunosController.Deletar);
 
 module.exports = router;
